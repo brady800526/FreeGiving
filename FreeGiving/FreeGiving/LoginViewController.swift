09da3f1de15312199f42ac62619869949bd3cdd7
brady800526 <brady800526@gmail.com>
2017-07-26 18:06:08 +0800

    finish prototype register page

---
+++ FreeGiving/FreeGiving/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  FreeGiving
+//
+//  Created by Brady Huang on 2017/7/26.
+//  Copyright © 2017年 AppWorks. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.cyan
+        
+
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FreeGiving/FreeGiving/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FreeGiving
+//
+//  Created by Brady Huang on 2017/7/26.
+//  Copyright © 2017年 AppWorks. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var logoutButtonItem: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+ 
+    func handleLogout() {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "loginPage") as! LoginViewController
+        self.present(vc, animated: true, completion: nil)
+
+    }
+    
+}
+
